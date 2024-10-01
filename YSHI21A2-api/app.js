0ca939ec472cc4eb83459ee4cf8a0f46cbe350fc
LunaY0229 <52578351+syzzys@users.noreply.github.com>
2024-10-01 22:40:50 +0800

    add search and getById API

--- YSHI21A2-api/app.js
+++ YSHI21A2-api/app.js
@@ -15,12 +15,45 @@ app.get('/activeFundraisers', (req, res) => {
   );
 })
 
+app.get('/searchFundraisers', (req, res) => {
+  const { ORGANIZER, CITY, CATEGORY_ID } = req.query
+
+  connection.execute(
+    `
+    SELECT * FROM fundraiser 
+    WHERE active=True 
+    ${ ORGANIZER ? ` AND ORGANIZER = "${ORGANIZER}"` : '' } 
+    ${ CITY ? ` AND CITY = "${CITY}"` : '' } 
+    ${ CATEGORY_ID ? ` AND CATEGORY_ID = "${CATEGORY_ID}"` : '' }
+  `,
+    function (err, results, fields) {
+      if (err) console.log(err)
+      res.json(results)
+    }
+  );
+})
+
 app.get('/categories', (req, res) => {
   connection.execute(
     'SELECT * FROM category',
     function (err, results, fields) {
       if (err) console.log(err)
       res.json(results)
+    }
+  );
+})
+
+app.get('/fundraisers/:id', (req, res) => {
+  connection.execute(
+    'SELECT * FROM fundraiser WHERE FUNDRAISER_ID = ?',
+    [req.params.id],
+    function (err, results, fields) {
+      if (err) console.log(err)
+      if (results.length > 0) {
+        res.json(results[0])
+      } else {
+        res.json({})
+      }
     }
   );
 })
