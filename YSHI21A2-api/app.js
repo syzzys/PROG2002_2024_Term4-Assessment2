cc2e5c8f90b524b380c08698b99ee0fdf49cd6e1
LunaY0229 <52578351+syzzys@users.noreply.github.com>
2024-10-01 22:30:52 +0800

    add get activeFundraisers and categories API

--- YSHI21A2-api/app.js
+++ YSHI21A2-api/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { getMysqlConnection } = require('./db/crowdfunding_db')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const connection = getMysqlConnection()
+
+app.get('/activeFundraisers', (req, res) => {
+  connection.execute(
+    'SELECT * FROM fundraiser WHERE active=True',
+    function (err, results, fields) {
+      if (err) console.log(err)
+      res.json(results)
+    }
+  );
+})
+
+app.get('/categories', (req, res) => {
+  connection.execute(
+    'SELECT * FROM category',
+    function (err, results, fields) {
+      if (err) console.log(err)
+      res.json(results)
+    }
+  );
 })
 
 app.listen(port, () => {
